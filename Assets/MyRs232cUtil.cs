bb6655d67fc10f2908ffbb72cf269fcbf92f42e9
y_okada <yasokada@gmail.com>
2015-09-20 07:15:08 +0900

    adding MyRs232cUtil

---
+++ Assets/MyRs232cUtil.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.IO.Ports; // for RS-232C
+
+namespace NS_MyRs232cUtil
+{
+	public static class MyRs232cUtil {
+		public static bool Open(string portname, out SerialPort sp) {
+			sp = new SerialPort ();
+			//      string[] portname = SerialPort.GetPortNames ();
+			//      sp.PortName = portname[0];
+
+			// portname : e.g. "COM3"
+			sp.PortName = portname;
+			sp.BaudRate = 9600;
+			sp.DataBits = 8;
+			sp.Parity = Parity.None;
+			sp.StopBits = StopBits.One;
+			sp.Handshake = Handshake.None;
+			sp.Encoding = System.Text.Encoding.ASCII;
+
+			try {
+				sp.Open ();
+			} catch(System.Exception e) {
+				Debug.LogWarning(e.Message);
+			}
+			return true;
+		}
+	}
+}
--- Assets/rs232cSendCS.cs
+++ Assets/rs232cSendCS.cs
@@ -3,21 +3,18 @@ using System.Collections;
 
 using System.IO.Ports; // for RS-232C
 
+using NS_MyRs232cUtil;
+
 public class rs232cSendCS : MonoBehaviour {
 
 	void Test_rs232c() {
-		SerialPort sp = new SerialPort ();
-		//      string[] portname = SerialPort.GetPortNames ();
-		//      sp.PortName = portname[0];
-		sp.PortName = "COM3";
-		sp.BaudRate = 9600;
-		sp.DataBits = 8;
-		sp.Parity = Parity.None;
-		sp.StopBits = StopBits.One;
-		sp.Handshake = Handshake.None;
-		sp.Encoding = System.Text.Encoding.ASCII;
-		
-		sp.Open ();
+		SerialPort sp;
+
+		bool res = MyRs232cUtil.Open ("COM3", out sp);
+		if (res == false) {
+			return; // fail
+		}
+
 		string msg = "hello";
 		sp.Write (msg);
 		sp.Close ();
