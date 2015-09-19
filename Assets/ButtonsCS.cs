094da3e253f616e9c783fe14f344805e415ec67a
y_okada <yasokada@gmail.com>
2015-09-20 07:34:35 +0900

    adding Start button

---
+++ Assets/ButtonsCS.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonsCS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartButtonClick() {
+		echoServerCS.SetStart ();
+	}
+	public void StopButtonClick() {
+		echoServerCS.SetStop ();
+	}
+}
---
+++ Assets/echoServerCS.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class echoServerCS : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public static void SetStart() {
+
+	}
+	public static void SetStop() {
+
+	}
+}
