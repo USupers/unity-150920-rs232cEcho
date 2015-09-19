702c2afa8ff19ea35c89b6e11236201f682929b7
y_okada <yasokada@gmail.com>
2015-09-20 07:40:11 +0900

    adding Thread

--- Assets/ButtonsCS.cs
+++ Assets/ButtonsCS.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class ButtonsCS : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 	
--- Assets/echoServerCS.cs
+++ Assets/echoServerCS.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using System.Threading; // for Thread
+
 public class echoServerCS : MonoBehaviour {
+
+	private static bool doStart = false;
+	private Thread rcvThr;
+	private bool stopThr = false;
 
 	void Start () {
 	
@@ -12,9 +18,28 @@ public class echoServerCS : MonoBehaviour {
 	}
 
 	public static void SetStart() {
-
+		doStart = true;
 	}
 	public static void SetStop() {
-
+		doStart = false;
 	}
+
+	void startThread() {
+		Debug.Log ("init");
+		rcvThr = new Thread (new ThreadStart (FuncRcvData));
+		rcvThr.Start ();
+	}
+	void stopThread() {
+		stopThr = true;
+		rcvThr.Abort ();
+	}
+
+	private void FuncRcvData() {
+		while (stopThr == false) {
+
+
+			Thread.Sleep(20);
+		}
+	}
+
 }
